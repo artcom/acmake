57b6078dac333a9ce9b28e87910a378b1c889d30
Claudia Winterstein <claudia.winterstein@artcom.de>
2013-02-22 15:17:40 +0100

    include path search order changed in find openCV

--- modules/FindOpenCV.cmake
+++ modules/FindOpenCV.cmake
@@ -1,19 +1,28 @@
 
-FIND_PATH(OPENCV_INCLUDE_DIR cv.h cxcore.h
-    PATH_SUFFIXES opencv
-    PATHS
-    "C:/OpenCV2.1/include"
-    /usr/local/include
-)
+if(OSX) 
+    FIND_PATH(OPENCV_INCLUDE_DIR cv.h cxcore.h
+        PATH_SUFFIXES opencv
+        PATHS
+        "C:/OpenCV2.1/include"
+        /usr/local/include/opencv
+        NO_DEFAULT_PATH 
+    )
+else()
+    FIND_PATH(OPENCV_INCLUDE_DIR cv.h cxcore.h
+        PATH_SUFFIXES opencv
+        PATHS
+        "C:/OpenCV2.1/include"
+        /usr/local/include/opencv
+    )
+endif(OSX)
 
 set(OPENCV_INCLUDE_DIRS ${OPENCV_INCLUDE_DIR})
 mark_as_advanced(OPENCV_DEFINITIONS OPENCV_INCLUDE_DIR OPENCV_INCLUDE_DIRS)
-
 if(WIN32) 
     set(OPENCV_SUBLIBS cv210.lib cvaux210.lib cxcore210.lib cxts210.lib highgui210.lib ml210.lib)
-else(OSX)
+elseif(OSX)
     set(OPENCV_SUBLIBS opencv_nonfree opencv_calib3d opencv_contrib opencv_core opencv_features2d opencv_flann opencv_gpu opencv_highgui opencv_imgproc opencv_legacy opencv_ml opencv_objdetect opencv_photo opencv_stitching opencv_ts opencv_video opencv_videostab)
-else(WIN32)
+else()
     set(OPENCV_SUBLIBS cv cvaux cxcore highgui ml)
 endif(WIN32)
 
@@ -29,9 +38,9 @@ endforeach(SUBLIB ${OPENCV_ALL_SUBLIBS})
 
 if(WIN32)
     set(OPENCV_SUBLIBS_D cv210d.lib cvaux210d.lib cxcore210d.lib cxts210d.lib highgui210d.lib ml210d.lib)
-else(OSX)
+elseif(OSX)
     set(OPENCV_SUBLIBS opencv_nonfree opencv_calib3d opencv_contrib opencv_core opencv_features2d opencv_flann opencv_gpu opencv_highgui opencv_imgproc opencv_legacy opencv_ml opencv_objdetect opencv_photo opencv_stitching opencv_ts opencv_video opencv_videostab)
-else(WIN32)
+else()
     set(OPENCV_SUBLIBS_D cv cvaux cxcore highgui ml)
 endif(WIN32)
 mark_as_advanced(OPENCV_SUBLIBS_D)
