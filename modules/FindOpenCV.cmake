f7b17e2441766784bd97a654d0611cd97af80bb0
Claudia Winterstein <claudiaw@puma.intern.artcom.de>
2013-03-05 12:03:54 +0100

    fixed cmake file

--- modules/FindOpenCV.cmake
+++ modules/FindOpenCV.cmake
@@ -1,4 +1,3 @@
-
 if(OSX) 
     FIND_PATH(OPENCV_INCLUDE_DIR cv.h cxcore.h
         PATH_SUFFIXES opencv
@@ -18,11 +17,12 @@ endif(OSX)
 
 set(OPENCV_INCLUDE_DIRS ${OPENCV_INCLUDE_DIR})
 mark_as_advanced(OPENCV_DEFINITIONS OPENCV_INCLUDE_DIR OPENCV_INCLUDE_DIRS)
+
 if(WIN32) 
     set(OPENCV_SUBLIBS cv210.lib cvaux210.lib cxcore210.lib cxts210.lib highgui210.lib ml210.lib)
-elseif(OSX)
-    set(OPENCV_SUBLIBS opencv_nonfree opencv_calib3d opencv_contrib opencv_core opencv_features2d opencv_flann opencv_gpu opencv_highgui opencv_imgproc opencv_legacy opencv_ml opencv_objdetect opencv_photo opencv_stitching opencv_ts opencv_video opencv_videostab)
-else()
+else(OSX)
+    set(OPENCV_SUBLIBS opencv_nonfree opencv_calib3d opencv_contrib opencv_core opencv_features2d opencv_flann opencv_gpu opencv_highgui opencv_imgproc opencv_legacy opencv_ml opencv_objdetect opencv_photo opencv_stitching opencv_ts opencv_video opencv_videostab cvblob)
+else(WIN32)
     set(OPENCV_SUBLIBS cv cvaux cxcore highgui ml)
 endif(WIN32)
 
@@ -38,9 +38,9 @@ endforeach(SUBLIB ${OPENCV_ALL_SUBLIBS})
 
 if(WIN32)
     set(OPENCV_SUBLIBS_D cv210d.lib cvaux210d.lib cxcore210d.lib cxts210d.lib highgui210d.lib ml210d.lib)
-elseif(OSX)
-    set(OPENCV_SUBLIBS opencv_nonfree opencv_calib3d opencv_contrib opencv_core opencv_features2d opencv_flann opencv_gpu opencv_highgui opencv_imgproc opencv_legacy opencv_ml opencv_objdetect opencv_photo opencv_stitching opencv_ts opencv_video opencv_videostab)
-else()
+else(OSX)
+    set(OPENCV_SUBLIBS_D opencv_nonfree opencv_calib3d opencv_contrib opencv_core opencv_features2d opencv_flann opencv_gpu opencv_highgui opencv_imgproc opencv_legacy opencv_ml opencv_objdetect opencv_photo opencv_stitching opencv_ts opencv_video opencv_videostab)
+else(WIN32)
     set(OPENCV_SUBLIBS_D cv cvaux cxcore highgui ml)
 endif(WIN32)
 mark_as_advanced(OPENCV_SUBLIBS_D)
